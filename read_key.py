08c27a4009ecece53bd3988e30ffe1be80dff97c
4johndoe <bogdandemchenko@gmail.com>
2014-09-21 11:18:56 +0300

    read_key

---
+++ read_key.py
@@ -0,0 +1,36 @@
+# Read form keyboard 
+# Show input 
+from livewires import games
+games.init(screen_width = 640, screen_height = 480, fps = 50)
+
+
+class Ship(games.Sprite):
+    """Moving starShip"""
+
+    def __init__(self):
+        pass
+    
+    def update(self):
+        """move spaceShip up to pressed key"""
+        if games.keyword.is_pressed(games.K_w):
+            self.y -= 1
+        if games.keyword.is_pressed(games.K_s):
+            self.y += 1
+        if games.keyword.is_pressed(games.K_a):
+            self.x -= 1
+        if games.keyword.is_pressed(games.K_d):
+            self.x += 1
+
+
+def main():
+    nebula_image = games.load_image("nebula.jpg", transparent = False)
+    games.screen.background = nebula_image
+    ship_image = games.load_image("ship.bmp")
+    the_ship = Ship(image = ship_image,
+        x = games.screen.width/2,
+        y = games.screen.height/2)
+    games.screen.add(the_ship)
+    games.screen.mainloop()
+
+
+main()
