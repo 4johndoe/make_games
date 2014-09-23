b9cb5398022b2be26ca601b302d1eb55a3075969
4johndoe <bogdandemchenko@gmail.com>
2014-09-23 21:25:09 +0300

    arcanoid1

--- arcanoid.py
+++ arcanoid.py
@@ -3,3 +3,20 @@
 import math, so, pygame, random
 from pygame.locals import *
 
+# game_constants
+SCREENRECT = Rect(0, 0, 640, 480)
+
+def load_sound(filename):
+    filename = os.path.join('data', filename)
+    return pygame.mixer.Sound(filename)
+
+def imgcolorkey(image, colorkey):
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image
+
+def load_image(filename, colorkey = None):
+    filename = os.path.join('data', filename)
+    
